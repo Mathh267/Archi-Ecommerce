f2b7a09a632f393c75be42f13c5a6b0a5146dedd
Mathéo Dhondt <38583978+Mathh267@users.noreply.github.com>
2024-04-17 14:51:37 +0200

    Ajout de la couche métier

---
+++ ecommerce/src/main/java/fr/univtours/polytech/ecommerce/business/PanierBusiness.java
@@ -0,0 +1,17 @@
+package fr.univtours.polytech.ecommerce.business;
+
+import java.util.List;
+
+import fr.univtours.polytech.ecommerce.model.ArticleBean;
+import fr.univtours.polytech.ecommerce.model.ArticlePanierBean;
+
+public interface PanierBusiness {
+
+    public List<ArticlePanierBean> addArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,ArticlePanierBean ArticlePanier);
+
+    public List<ArticlePanierBean> addQuantiteArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,ArticleBean Article,Integer Quantité);
+
+    public List<ArticlePanierBean> delQuantiteArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,ArticleBean Article,Integer Quantité);
+
+    public Float ComputeTotalArticlePanier(List<ArticlePanierBean> ListeArticlePanier);
+}
---
+++ ecommerce/src/main/java/fr/univtours/polytech/ecommerce/business/PanierBusinessImpl.java
@@ -0,0 +1,47 @@
+package fr.univtours.polytech.ecommerce.business;
+
+import java.util.List;
+
+import fr.univtours.polytech.ecommerce.model.ArticleBean;
+import fr.univtours.polytech.ecommerce.model.ArticlePanierBean;
+
+public class PanierBusinessImpl implements PanierBusiness {
+
+    @Override
+    public Float ComputeTotalArticlePanier(List<ArticlePanierBean> ListeArticlePanier) {
+        if (ListeArticlePanier.isEmpty()){
+            return 0F;
+        }
+        else {
+            float somme=0F;
+            for (ArticlePanierBean ArticlePanier : ListeArticlePanier){
+                somme=somme+ArticlePanier.getQuantitéPanier()*ArticlePanier.getArticleBean().getPrix();
+            }
+            return somme;
+        }
+    }
+
+    @Override
+    public List<ArticlePanierBean> addArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,
+            ArticlePanierBean ArticlePanier) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<ArticlePanierBean> addQuantiteArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,
+            ArticleBean Article, Integer Quantité) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<ArticlePanierBean> delQuantiteArticlePanierBean(List<ArticlePanierBean> ListeArticlePanier,
+            ArticleBean Article, Integer Quantité) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
+}
